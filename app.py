605d8b3079fde20674ea6b559206157ba535255c
Ellamea <gallimbaellamea@isufst.edu.ph>
2025-10-30 01:39:44 +0800

    Update app.py
    Signed-off-by: Ellamea <gallimbaellamea@isufst.edu.ph>

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import date
 
-# ---------------- Flask Setup ----------------
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -13,87 +12,80 @@ class Student(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     fullname = db.Column(db.String(100), nullable=False)
     address = db.Column(db.String(200))
-    birthday = db.Column(db.String(20))
+    birthday = db.Column(db.Date)
     age = db.Column(db.Integer)
     sex = db.Column(db.String(10))
-    course = db.Column(db.String(10))
-    section = db.Column(db.String(20))
-    email = db.Column(db.String(100), unique=True)
+    course = db.Column(db.String(50))
+    section = db.Column(db.String(50))
+    email = db.Column(db.String(100))
     hobby = db.Column(db.String(100))
     contact_number = db.Column(db.String(20))
     guardian_name = db.Column(db.String(100))
     guardian_contact = db.Column(db.String(20))
 
-    def to_dict(self):
-        return {col.name: getattr(self, col.name) for col in self.__table__.columns}
-
-with app.app_context():
-    db.create_all()
-
 # ---------------- Routes ----------------
-
-# First page: Add student if none exist, else dashboard
-@app.route('/')
-def first_student_form():
-    if Student.query.count() == 0:
-        return redirect(url_for('add_student'))
-    else:
-        return redirect(url_for('index'))
-
-# Dashboard
-@app.route('/index')
+@app.route('/', methods=['GET'])
 def index():
-    students = Student.query.all()
+    search_query = request.args.get('search', '')
+    if search_query:
+        students = Student.query.filter(Student.fullname.ilike(f"%{search_query}%")).all()
+    else:
+        students = Student.query.all()
     return render_template('index.html', students=students)
 
-# Search
-@app.route('/search', methods=['GET'])
-def search_student():
-    query = request.args.get('q', '')
-    students = Student.query.filter(Student.fullname.ilike(f'%{query}%')).all()
-    return render_template('index.html', students=students, search_query=query)
-
-# Add student
 @app.route('/add', methods=['GET', 'POST'])
 def add_student():
     if request.method == 'POST':
-        data = request.form
-        birthday = data['birthday']
-        age = date.today().year - int(birthday.split('-')[0])
-        new_student = Student(
-            fullname=data['fullname'], address=data['address'], birthday=birthday,
-            age=age, sex=data['sex'], course=data['course'], section=data['section'],
-            email=data['email'], hobby=data['hobby'], contact_number=data['contact_number'],
-            guardian_name=data['guardian_name'], guardian_contact=data['guardian_contact']
+        birthday = request.form['birthday']
+        birth_date = date.fromisoformat(birthday)
+        today = date.today()
+        age = today.year - birth_date.year - ((today.month, today.day) < (birth_date.month, birth_date.day))
+
+        student = Student(
+            fullname=request.form['fullname'],
+            address=request.form['address'],
+            birthday=birth_date,
+            age=age,
+            sex=request.form['sex'],
+            course=request.form['course'],
+            section=request.form['section'],
+            email=request.form['email'],
+            hobby=request.form['hobby'],
+            contact_number=request.form['contact_number'],
+            guardian_name=request.form['guardian_name'],
+            guardian_contact=request.form['guardian_contact']
         )
-        db.session.add(new_student)
+        db.session.add(student)
         db.session.commit()
         return redirect(url_for('index'))
     return render_template('form.html', student=None)
 
-# Edit student
 @app.route('/edit/<int:id>', methods=['GET', 'POST'])
 def edit_student(id):
     student = Student.query.get_or_404(id)
     if request.method == 'POST':
-        data = request.form
-        student.fullname = data['fullname']
-        student.address = data['address']
-        student.birthday = data['birthday']
-        student.age = date.today().year - int(student.birthday.split('-')[0])
-        student.sex = data['sex']
-        student.course = data['course']
-        student.section = data['section']
-        student.email = data['email']
-        student.hobby = data['hobby']
-        student.contact_number = data['contact_number']
-        student.guardian_name = data['guardian_name']
-        student.guardian_contact = data['guardian_contact']
+        birthday = request.form['birthday']
+        birth_date = date.fromisoformat(birthday)
+        today = date.today()
+        age = today.year - birth_date.year - ((today.month, today.day) < (birth_date.month, birth_date.day))
+
+        student.fullname = request.form['fullname']
+        student.address = request.form['address']
+        student.birthday = birth_date
+        student.age = age
+        student.sex = request.form['sex']
+        student.course = request.form['course']
+        student.section = request.form['section']
+        student.email = request.form['email']
+        student.hobby = request.form['hobby']
+        student.contact_number = request.form['contact_number']
+        student.guardian_name = request.form['guardian_name']
+        student.guardian_contact = request.form['guardian_contact']
+
         db.session.commit()
         return redirect(url_for('index'))
     return render_template('form.html', student=student)
 
-# Delete student
 @app.route('/delete/<int:id>')
 def delete_student(id):
     student = Student.query.get_or_404(id)
@@ -101,16 +93,6 @@ def delete_student(id):
     db.session.commit()
     return redirect(url_for('index'))
 
-# ---------------- API Endpoints ----------------
-@app.route('/api/students', methods=['GET'])
-def api_get_students():
-    return jsonify([s.to_dict() for s in Student.query.all()])
-
-@app.route('/api/students/<int:id>', methods=['GET'])
-def api_get_student(id):
-    student = Student.query.get_or_404(id)
-    return jsonify(student.to_dict())
-
-# ---------------- Run App ----------------
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    db.create_all()
+    app.run(debug=True)
