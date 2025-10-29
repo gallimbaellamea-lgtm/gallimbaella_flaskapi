fa22c4fb1614ebd19387fe9e9fb93866d2d1285f
Ellamea <gallimbaellamea@isufst.edu.ph>
2025-10-29 23:13:03 +0800

    Update app.py
    Signed-off-by: Ellamea <gallimbaellamea@isufst.edu.ph>

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, jsonify
 from models import db, Student
 from forms import StudentForm
 
@@ -13,57 +13,89 @@ db.init_app(app)
 def create_tables():
     db.create_all()
 
-# Create + Read
+# ---------- HTML Form Routes ----------
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = StudentForm()
     if form.validate_on_submit():
-        new_student = Student(
+        student = Student(
             full_name=form.full_name.data,
             birthday=form.birthday.data,
+            sex=form.sex.data,
             course=form.course.data,
             section=form.section.data,
-            sex=form.sex.data,
             email=form.email.data,
             address=form.address.data,
-            hobby=form.hobby.data
+            hobby=form.hobby.data,
+            contact_number=form.contact_number.data,
+            guardian_name=form.guardian_name.data,
+            guardian_contact=form.guardian_contact.data
         )
-        db.session.add(new_student)
+        db.session.add(student)
         db.session.commit()
         return redirect(url_for('dashboard'))
     return render_template('index.html', form=form)
 
-# Read Dashboard
 @app.route('/dashboard')
 def dashboard():
     students = Student.query.all()
     return render_template('dashboard.html', students=students)
 
-# Update
 @app.route('/edit/<int:id>', methods=['GET', 'POST'])
 def edit_student(id):
     student = Student.query.get_or_404(id)
     form = StudentForm(obj=student)
     if form.validate_on_submit():
-        student.full_name = form.full_name.data
-        student.birthday = form.birthday.data
-        student.course = form.course.data
-        student.section = form.section.data
-        student.sex = form.sex.data
-        student.email = form.email.data
-        student.address = form.address.data
-        student.hobby = form.hobby.data
+        form.populate_obj(student)
         db.session.commit()
         return redirect(url_for('dashboard'))
     return render_template('index.html', form=form, edit=True)
 
-# Delete
 @app.route('/delete/<int:id>', methods=['POST'])
 def delete_student(id):
     student = Student.query.get_or_404(id)
     db.session.delete(student)
     db.session.commit()
     return redirect(url_for('dashboard'))
+
+# ---------- API Routes ----------
+@app.route('/api/students', methods=['GET'])
+def api_students():
+    students = Student.query.all()
+    return jsonify([{
+        "id": s.id,
+        "full_name": s.full_name,
+        "age": s.age,
+        "birthday": s.birthday.strftime("%Y-%m-%d"),
+        "sex": s.sex,
+        "course": s.course,
+        "section": s.section,
+        "email": s.email,
+        "address": s.address,
+        "hobby": s.hobby,
+        "contact_number": s.contact_number,
+        "guardian_name": s.guardian_name,
+        "guardian_contact": s.guardian_contact
+    } for s in students])
+
+@app.route('/api/student/<int:id>', methods=['GET'])
+def api_student(id):
+    s = Student.query.get_or_404(id)
+    return jsonify({
+        "id": s.id,
+        "full_name": s.full_name,
+        "age": s.age,
+        "birthday": s.birthday.strftime("%Y-%m-%d"),
+        "sex": s.sex,
+        "course": s.course,
+        "section": s.section,
+        "email": s.email,
+        "address": s.address,
+        "hobby": s.hobby,
+        "contact_number": s.contact_number,
+        "guardian_name": s.guardian_name,
+        "guardian_contact": s.guardian_contact
+    })
 
 if __name__ == '__main__':
     app.run(debug=True)
