97f30583561cea27a903042242d86e2a5f0d589e
Ellamea <gallimbaellamea@isufst.edu.ph>
2025-10-29 23:11:26 +0800

    Update models.py
    Signed-off-by: Ellamea <gallimbaellamea@isufst.edu.ph>

--- models.py
+++ models.py
@@ -7,12 +7,15 @@ class Student(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     full_name = db.Column(db.String(100), nullable=False)
     birthday = db.Column(db.Date, nullable=False)
+    sex = db.Column(db.String(10), nullable=False)
     course = db.Column(db.String(10), nullable=False)
     section = db.Column(db.String(10), nullable=False)
-    sex = db.Column(db.String(10), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     address = db.Column(db.String(200), nullable=False)
     hobby = db.Column(db.String(200))
+    contact_number = db.Column(db.String(20))
+    guardian_name = db.Column(db.String(100))
+    guardian_contact = db.Column(db.String(20))
 
     @property
     def age(self):
